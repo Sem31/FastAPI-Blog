5c52b7f3c8dd06be10550d0522c24d66ee598c41
Sem31 <31kamleshprajapat@gmail.com>
2025-09-30 02:55:14 +0530

    creating pydantic model and trying to use it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -9,6 +10,27 @@ def index():
     return {"Hello": "World"}
 
 
+@app.get("/blog/{blog_id}")
+def get_blog(blog_id: int):
+    return {"data": "Blog with id: {}".format(blog_id)}
+
+
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int):
+    return {"data": "Blog with id: {} comment".format(blog_id)}
+
+
 @app.get("/about")
 def about():
     return {"data": "About data page"}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: bool = False
+
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {"data": "Blog is created with title {}".format(blog.title)}
